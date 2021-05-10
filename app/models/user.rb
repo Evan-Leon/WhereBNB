7c28be4f7258f368135c83e5854f5ee62c994f96
Evan-Leon <evan.leon.j@gmail.com>
2021-05-10 14:38:45 -0400

    Finished user model, created api users_controller and backendroutes for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,40 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true}
 
   after_initialize :ensure_session_token
+
+  attr_reader :password 
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+        return user 
+    else  
+        return nil 
+    end
+  end
+
+  def self.generate_session_token 
+    SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password 
+  end
+
+  def is_password?(password)
+    pass_obj = BCrypt::Password.new(self.password_digest)
+    pass_obj.is_password?(password)
+  end
+
+  def ensure_session_token 
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
 end
