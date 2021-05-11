0cc5c97e5ef70f025f2ec9faa7efd5cd51f34314
Evan-Leon <evan.leon.j@gmail.com>
2021-05-11 10:25:08 -0400

    Adding frontend actions and reducers for users

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
+import NavBarContainer from './nav_bar/nav_bar_container';
+import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
 
 const App = () => (
   <div>
