e6fc8912e1c4fce32fb752ed38ec26f2ec4870e2
Evan-Leon <evan.leon.j@gmail.com>
2021-05-13 10:31:46 -0400

    Added listings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_140846) do
+ActiveRecord::Schema.define(version: 2021_05_13_141618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,20 @@ ActiveRecord::Schema.define(version: 2021_05_13_140846) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.text "title", null: false
+    t.integer "price", null: false
+    t.integer "guest_num", null: false
+    t.text "description", null: false
+    t.integer "num_beds", null: false
+    t.float "longitude", null: false
+    t.float "latitude", null: false
+    t.integer "host_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["host_id"], name: "index_listings_on_host_id"
   end
 
   create_table "users", force: :cascade do |t|
