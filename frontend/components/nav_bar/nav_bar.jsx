4f16a14b53872718104013ef36933be0f757421e
Evan-Leon <evan.leon.j@gmail.com>
2021-05-14 14:56:02 -0400

    Fixed search bar error

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Search from './search'
+import Dropdown from './dropdown'
 
 const NavBar = ({currentUser, demoUser, logout, openModal, closeModal, login }) => {
     
     const navSessionLinks = () =>(
         <nav className="login-signup">
+            {/* <Dropdown className="dropdown" /> */}
             <button onClick={() => openModal('login')}>Login</button>
             <button onClick={() => openModal('signup')}>Signup</button>
             <button onClick={() => login(demoUser)}>Demo User</button>
--- frontend/components/nav_bar/search.jsx
+++ frontend/components/nav_bar/search.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class Search extends React.Component{
-
+    constructor(props){
+        super(props);
+        this.state = {
+            search: '',
+        }
+    }
+    update(){
+        return e => this.setState({search: e.currentTarget.value })
+    }
 
     render(){
         return(
             <div className="search-bar-box">
                 <form action="">
-                    <input type="text" className="search-text" value="Search..." />
+                    <input type="text" className="search-text" value={this.state.search} onChange={this.update} />
                 </form>
             </div>
         )
