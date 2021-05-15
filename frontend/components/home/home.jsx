b90de280b8d500c6ff88d9c468c509694841d191
Evan-Leon <evan.leon.j@gmail.com>
2021-05-15 14:03:29 -0400

    Positioned dropdown, header and main content for home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -12,9 +12,26 @@ class Home extends React.Component {
 
         return(
             <div className="main-body">
-                [BodyBanner] 
-                [Explore Nearby]
-                [Visit Anywhere]
+                <h2>BANNER PICTURE WITH BUTTON</h2> 
+                <br />
+                <h2>Explore</h2>
+                <ul className="explore-container">
+                    <li>New York City</li>
+                    <li>San Francisco</li>
+                    <li>Boston</li>
+                    <li>Chicago</li>
+                    <li>Miami</li>
+                    <li>Seattle</li>
+                </ul>
+                <br />
+                <h2 className="live-anywhere">Live Anywhere</h2>
+                <ul className="visit-container">
+                    <li>Outdoor getaways</li>
+                    <li>Unique stays</li>
+                    <li>Entire Homes</li>
+                    <li>Pets Allowed</li>
+                </ul>
+                <br />
                 [Become Host/ Discover Experiences?]
             </div>
         )
